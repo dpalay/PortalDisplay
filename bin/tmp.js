51cfcf794f5ce341a4f62bd8e71951bc2205e6dd
Dave Palay <dpalay@epic.com>
2019-09-20 16:31:57 -0500

    updated data; started to get multiple image page

--- bin/tmp.js
+++ bin/tmp.js
@@ -1,4 +1,11 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -63,7 +70,7 @@ var portals = sortedList.map(function (portal) {
         slopeFromAnchor: slope(anchor.coordinates, portal.coordinates) };
 }).sort(function (a, b) { return b.slopeFromAnchor - a.slopeFromAnchor; });
 // Add the portals to the output Portal List
-output.portalList = [output.anchor].concat(portals);
+output.portalList = __spreadArrays([output.anchor], portals);
 output.betterPortalList =
     [
         { portal: output.anchor, linksFrom: [], linksTo: [] }
@@ -79,14 +86,14 @@ allLinks = portals.map(function (portal) {
         dest: { title: anchor.title, x: anchor.coordinates.lng, y: anchor.coordinates.lat }
     };
 });
-output.allLinks = allLinks.slice();
+output.allLinks = __spreadArrays(allLinks);
 // all these links go TO the anchor
 output.linksTo = [];
 for (var index = 0; index < portals.length + 1; index++) {
     output.linksTo[index] = [];
 }
-output.linksTo[0] = allLinks.slice();
-output.betterPortalList[0].linksTo = allLinks.slice();
+output.linksTo[0] = __spreadArrays(allLinks);
+output.betterPortalList[0].linksTo = __spreadArrays(allLinks);
 // add each link to the linksFrom
 output.linksFrom = [];
 for (var index = 0; index < portals.length + 1; index++) {
@@ -95,8 +102,8 @@ for (var index = 0; index < portals.length + 1; index++) {
 }
 for (var index = 0; index < allLinks.length; index++) {
     var link = allLinks[index];
-    output.linksFrom[index + 1] = [link].concat(output.linksFrom[index + 1]);
-    output.betterPortalList[index + 1].linksFrom = [link].concat(output.betterPortalList[index + 1].linksFrom);
+    output.linksFrom[index + 1] = __spreadArrays([link], output.linksFrom[index + 1]);
+    output.betterPortalList[index + 1].linksFrom = __spreadArrays([link], output.betterPortalList[index + 1].linksFrom);
 }
 /*
 portals:  [ A, b, c, d]
@@ -132,7 +139,7 @@ for (var sourcePortalIndex = 1; sourcePortalIndex < portals.length; sourcePortal
         _loop_1(destPortalIndex);
     }
 }
-output.allLinks = allLinks.slice();
+output.allLinks = __spreadArrays(allLinks);
 //console.log(output)
 fs.writeFileSync('./tmpoutput.json', JSON.stringify(output));
 console.log(output.linksTo.map(function (x, i) { return output.portalList[i].title + "," + x.length; }));
